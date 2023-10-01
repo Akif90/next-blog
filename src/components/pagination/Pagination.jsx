b1fef4b7dc7c17deab38d7ae805d831018fd08f3
Akif Ahmed <akifahmed90@gmail.com>
2023-10-01 23:09:47 +0530

    Created Pagination component

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./pagination.module.css";
 
 const Pagination = () => {
-  return <div className={styles.container}>Pagination</div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.btn}>Prev</button>
+      <button className={styles.btn}>Next</button>
+    </div>
+  );
 };
 
 export default Pagination;

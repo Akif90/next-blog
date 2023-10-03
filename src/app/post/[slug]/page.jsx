fd7ebd037d983c99659ecde55392fa22fc81b23b
Akif Ahmed <akifahmed90@gmail.com>
2023-10-03 22:39:55 +0530

    created API for single post

---
+++ src/app/post/[slug]/page.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./singlePage.module.css";
+import Menu from "@/components/menu/Menu";
+import Image from "next/image";
+import Comment from "@/components/comment/Comment";
+
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-cache",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failure");
+  }
+
+  return res.json();
+};
+const SinglePage = async ({params}) => {
+  const {slug} = params;
+  const data = await getData(slug);
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>{data?.title}</h1>
+          <div className={styles.user}>
+            {data?.user?.img && (
+              <div className={styles.userImageContainer}>
+                <Image
+                  src={data.user.image}
+                  alt=""
+                  fill
+                  className={styles.avatar}
+                />
+              </div>
+            )}
+            <div className={styles.userTextContainer}>
+              <span className={styles.username}>{data?.user.name}</span>
+              <span className={styles.date}>21.04.21</span>
+            </div>
+          </div>
+        </div>
+
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data.img} alt="" fill className={styles.image} />
+          </div>
+        )}
+      </div>
+      <div className={styles.content}>
+        <div className={styles.posts}>
+          <div
+            className={styles.desc}
+            dangerouslySetInnerHTML={{__html: data?.desc}}
+          ></div>
+          <div className={styles.comment}>
+            <Comment />
+          </div>
+        </div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,24 +6,23 @@ import Link from "next/link";
 const Card = ({key, item}) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image className={styles.image} src={item.img} alt="" fill />
-      </div>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image className={styles.image} src={item.img} alt="" fill />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
           <span className={styles.date}>{item.createdAt.substring(0, 10)}</span>
           <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href="/">
+        <Link href={`/post/${item.slug}`}>
           <h1 className={styles.title}>
             Lorem ipsum dolor sit amet consectetur
           </h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-          dolores porro error numquam, sed voluptatum
-        </p>
-        <Link href="/" className={styles.link}>
+        <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+        <Link href={`/post/${item.slug}`} className={styles.link}>
           Read More.
         </Link>
       </div>
